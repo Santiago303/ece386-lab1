4311478bedbd0c17fb23c6af976b0e9935db4db2
Santiago <Santi.gutierrez303@gmail.com>
2025-01-30 10:35:00 -0700

    formatted

--- client/client.py
+++ client/client.py
@@ -13,8 +13,10 @@ def get_img_prediction(
 
     image_path = Path(image_path)
     url = f"http://{server_ip}:{server_port}/{api_path}"
-    with image_path.open('rb') as img_file:
-        files = {'file': (image_path.name, img_file, 'image/png')}  # Assuming PNG, change MIME type as needed
+    with image_path.open("rb") as img_file:
+        files = {
+            "file": (image_path.name, img_file, "image/png")
+        }  # Assuming PNG, change MIME type as needed
         response = requests.post(url, files=files)
     return response.text
 
@@ -26,13 +28,15 @@ def main(server_ip: str, server_port: int) -> None:
     """
     while True:
         image_path = input("Enter the path to the image or 'exit' to quit: ")
-        if image_path.lower() == 'exit':
+        if image_path.lower() == "exit":
             break
         if not Path(image_path).is_file():
             print("File does not exist, please try again.")
             continue
-        
-        response = get_img_prediction(server_ip, server_port, "predict", Path(image_path))
+
+        response = get_img_prediction(
+            server_ip, server_port, "predict", Path(image_path)
+        )
         print("Prediction from server:", response)
 
     print(f"Using server {server_ip}:{server_port}")
